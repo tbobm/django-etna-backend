25d053ad6153cf41d4d680def9d27ba3d4db0bff
Theo Massard <theo@memento-solution.com>
2020-11-12 22:38:03 +0100

    Implement authentication based on etnawrapper

--- etna_backend/EtnaApiBackend.py
+++ etna_backend/EtnaApiBackend.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 """
-Django backend requesting ETNA APIs for auth.
+Django backend querying ETNA APIs for authentication.
 
-User existence and registration is based on the API request
+User existence and registration is based on the API result.
 """
 from django.contrib.auth.models import User
 from etnawrapper import EtnaWrapper
@@ -11,8 +10,31 @@ from etnawrapper import EtnaWrapper
 class EtnaAPIBackend:
     """Authenticate through the ETNA API."""
 
+    # pylint: disable=no-self-use,unused-argument
     def authenticate(self, request, username=None, password=None):
         """Make a request to the API and check for the return code."""
         client = EtnaWrapper(username=username, password=password)
-        infos = client.get_infos()
+        try:
+            infos = client.get_user_infos()
+            # TODO: Use proper exception handling
+        except Exception as err:
+            print(f'{username}: {err}')
+            return None
+        username = infos['login']
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:  # pylint: disable=no-member
+            # Create a new user. There's no need to set a password
+            # because only the password from settings.py is checked.
+            user = User(pk=infos['id'], username=username, email=infos['email'])
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+        return user
 
+    def get_user(self, user_id):
+        """Fetch a user based on its user_id."""
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:  # pylint: disable=no-member
+            return None
